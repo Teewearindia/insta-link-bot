8ca5e6a63e4a9a19e8cfee60322a2a437d8b72e2
Anjali Chavan <teewear2250@gmail.com>
2025-04-07 18:28:02 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,4 +58,4 @@ def send_dm(user_id, text):
     print(f"📬 DM Sent: {response.status_code} - {response.text}")
 
 if __name__ == "__main__":
-    app.run(port=5000)
+    app.run(host="0.0.0.0", port=10000)
